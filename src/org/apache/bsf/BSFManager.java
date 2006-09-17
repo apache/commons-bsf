5165a962d9bae8168708f25b47898d1d67dd2864
Rony Flatscher <rony@apache.org>
2006-09-17 15:17:50 +0000

    20060917 16:17 Changed version string to reflect actual version and date.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bsf/trunk@447077 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/apache/bsf/BSFManager.java
+++ src/org/apache/bsf/BSFManager.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2004,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -59,7 +59,7 @@ public class BSFManager {
     // version string in the form of: "abb.yyyymmdd",
     // where "a" is the major version number, "bb" the minor version number,
     // and "yyyymmdd" represents the date in sorted order (four digit year, two digit month, two digit day)
-    protected static String version="205.20060327";
+    protected static String version="240.20060917";
 
     // table of registered scripting engines
     protected static Hashtable registeredEngines = new Hashtable();
