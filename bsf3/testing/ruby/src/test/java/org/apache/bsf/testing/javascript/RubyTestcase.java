a2fd29ff6f36db3ae8f9969265eb04598a81850f
Anthony Elder <antelder@apache.org>
2007-03-10 11:44:48 +0000

    Add a Ruby testcase using the JRuby script engine
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bsf/trunk@516687 13f79535-47bb-0310-9956-ffa450edef68

---
+++ bsf3/testing/ruby/src/test/java/org/apache/bsf/testing/javascript/RubyTestcase.java
@@ -0,0 +1,40 @@
+package org.apache.bsf.testing.javascript;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+import junit.framework.TestCase;
+
+
+public class RubyTestcase extends TestCase {
+	
+//	public void testEval() throws ScriptException {
+//		ScriptEngineManager manager = new ScriptEngineManager();
+//		ScriptEngine engine = manager.getEngineByExtension("js");
+//		assertTrue(((Boolean)engine.eval("true;")).booleanValue());
+//		assertFalse(((Boolean)engine.eval("false;")).booleanValue());
+//	}
+
+	public void testInvokeFunction() throws ScriptException {
+		ScriptEngineManager manager = new ScriptEngineManager();
+		ScriptEngine engine = manager.getEngineByExtension("rb");
+		engine.eval("def hello(s)\n   return \"Hello \" + s\nend" );
+		assertTrue(engine instanceof Invocable);
+		Invocable invocableScript = (Invocable) engine;
+		assertEquals("Hello petra", invocableScript.invokeFunction("hello", new Object[]{"petra"}));
+	}
+
+//	public void testInvokeMethod() throws ScriptException {
+//		ScriptEngineManager manager = new ScriptEngineManager();
+//		ScriptEngine engine = manager.getEngineByExtension("js");
+//		engine.eval("function hello(s) { return 'Hello ' + s; }" );
+//		assertTrue(engine instanceof Invocable);
+//		Invocable invocableScript = (Invocable) engine;
+//		
+//		Object thiz = engine.eval("this;");
+//		assertEquals("Hello petra", invocableScript.invokeMethod(thiz, "hello", new Object[]{"petra"}));
+//	}
+
+}
