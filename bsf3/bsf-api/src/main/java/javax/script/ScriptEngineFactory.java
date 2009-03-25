5bcc57b751a11cfd76b1926f4876721dcae54f2f
Sebastian Bazley <sebb@apache.org>
2009-03-25 02:04:17 +0000

    More Javadoc
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/bsf/trunk@758126 13f79535-47bb-0310-9956-ffa450edef68

--- bsf3/bsf-api/src/main/java/javax/script/ScriptEngineFactory.java
+++ bsf3/bsf-api/src/main/java/javax/script/ScriptEngineFactory.java
@@ -99,9 +99,66 @@ public interface ScriptEngineFactory {
      */
     public Object getParameter(String key);    
 
-    public String getMethodCallSyntax(String obj, String method, String[] args);
+    /**
+     * Returns a String which can be used to invoke a method of a Java object 
+     * using the syntax of the supported scripting language. 
+     * For instance, an implementaton for a Javascript engine might be;
+     * <pre>
+     *  public String getMethodCallSyntax(String objectName,
+     *                                    String method, String[] args) {
+     *       String ret = objectName;
+     *       ret += "." + method + "(";
+     *       for (int i = 0; i < args.length; i++) {
+     *           ret += args[i];
+     *           if (i == args.length - 1) {
+     *               ret += ")";
+     *           } else {
+     *               ret += ",";
+     *           }
+     *       }
+     *       return ret;
+     *  }
+     * </pre>
+     * @param objectName The name representing the object whose method is to be invoked.
+     * @param method The name of the method to invoke.
+     * @param args names of the arguments in the method call. 
+     * @return The String used to invoke the method in the syntax of the scripting language.
+     */
+    public String getMethodCallSyntax(String objectName, String method, String[] args);
 
+    /**
+     * Returns a String that can be used as a statement to display 
+     * the specified String using the syntax of the supported 
+     * scripting language. For instance, the implementaton for a 
+     * Perl engine might be;
+     * <pre> 
+     * public String getOutputStatement(String toDisplay) {
+     *       return "print(" + toDisplay + ")";
+     *  }
+     *  </pre>
+     * @param toDisplay the string to be displayed
+     * 
+     * @return The string used to display the String in the syntax of 
+     * the scripting language.
+     */
     public String getOutputStatement(String toDisplay);
 
+    /**
+     * Returns A valid scripting language executable progam with 
+     * given statements. For instance an implementation for a PHP 
+     * engine might be:
+     * <pre> 
+     *  public String getProgram(String... statements) {
+     *       $retval = "<?\n";
+     *       int len = statements.length;
+     *       for (int i = 0; i < len; i++) {
+     *           $retval += statements[i] + ";\n";
+     *       }
+     *       $retval += "?>";
+     *  }
+     * </pre>
+     * @param statements
+     * @return The Program
+     */
     public String getProgram(String[] statements);
 }
